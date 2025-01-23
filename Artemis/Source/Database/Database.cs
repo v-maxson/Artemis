4c647aecff0372c022c678a108b233e6de8c5198
Graeme Morris <vmaxson.dev@gmail.com>
2025-01-23 15:57:24 -0700

    feat: implement daily database backups

--- Artemis/Source/Database/Database.cs
+++ Artemis/Source/Database/Database.cs
@@ -4,10 +4,23 @@ namespace Database;
 
 internal static class Database
 {
-    public const string DB_CONNECTION_STRING = "Filename=database.db;Connection=shared;";
+    public const string DB_FILE_NAME = "database";
+    public const string DB_FILE_EXT = ".db";
+    public const string DB_FILE = $"{DB_FILE_NAME}{DB_FILE_EXT}";
+    public const string DB_CONNECTION_STRING = $"Filename={DB_FILE};Connection=shared;";
 
     public static LiteDatabase Connect()
     {
         return new LiteDatabase(DB_CONNECTION_STRING);
+    }
+
+    public static void CreateBackup()
+    {
+        // If "backups" folder doesn't exist, create it.
+        if (!Directory.Exists("backups"))
+            Directory.CreateDirectory("backups");
+
+        var backupFilePath = $"./backups/{DB_FILE_NAME}.{DateTime.Now:yyyy-MM-dd.h-mm-ss}{DB_FILE_EXT}";
+        File.Copy(DB_FILE, backupFilePath);
     }
 }
--- Artemis/Source/Handlers/ReadyHandler.cs
+++ Artemis/Source/Handlers/ReadyHandler.cs
@@ -21,9 +21,14 @@ public class ReadyHandler(GatewayClient client, Config config, Stopwatch stopwat
         Stopwatch.Start();
 
         Log.Information("Connected to Discord as {Username}#{Discriminator}", ready.User.Username, ready.User.Discriminator);
-
+        
+        // Set a random presence.
         await SetRandomPresenceAsync();
+
+        // Hangfire Jobs.
         RecurringJob.AddOrUpdate("SetRandomPresence", () => SetRandomPresenceAsync(), Cron.Minutely);
+        Database.Database.CreateBackup();
+        RecurringJob.AddOrUpdate("Database Backup", () => Database.Database.CreateBackup(), Cron.Daily);
     }
 
     public async Task SetRandomPresenceAsync()
