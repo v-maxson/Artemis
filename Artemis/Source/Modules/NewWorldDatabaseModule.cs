bfc8184ee46a22a885df37525061bb0fe0c3f86e
Graeme Morris <vmaxson.dev@gmail.com>
2025-01-22 21:58:39 -0700

    feat: add all nwdb commands

--- Artemis/Source/Modules/NewWorldDatabaseModule.cs
+++ Artemis/Source/Modules/NewWorldDatabaseModule.cs
@@ -5,15 +5,150 @@ using NetCord.Services.ApplicationCommands;
 
 namespace Modules;
 
-public class ItemAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+[SlashCommand("nwdb", "New World Database commands.")]
+public partial class NewWorldDatabaseModule : ApplicationCommandModule<ApplicationCommandContext>
+{
+    [SubSlashCommand("item", "Search for an item in the New World Database.")]
+    public async Task ItemAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the item to search for.", AutocompleteProviderType = typeof(ItemsAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("mount", "Search for a mount in the New World Database.")]
+    public async Task MountAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the mount to search for.", AutocompleteProviderType = typeof(MountsAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("recipe", "Search for a recipe in the New World Database.")]
+    public async Task RecipeAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the recipe to search for.", AutocompleteProviderType = typeof(RecipesAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("ability", "Search for an ability in the New World Database.")]
+    public async Task AbilityAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the ability to search for.", AutocompleteProviderType = typeof(AbilitiesAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("perk", "Search for a perk in the New World Database.")]
+    public async Task PerkAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the perk to search for.", AutocompleteProviderType = typeof(PerksAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("status-effect", "Search for a status effect in the New World Database.")]
+    public async Task StatusEffectAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the status effect to search for.", AutocompleteProviderType = typeof(StatusEffectsAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("quest", "Search for a quest in the New World Database.")]
+    public async Task QuestAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the quest to search for.", AutocompleteProviderType = typeof(QuestsAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("creature", "Search for a creature in the New World Database.")]
+    public async Task CreatureAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the creature to search for.", AutocompleteProviderType = typeof(CreaturesAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("gatherable", "Search for a gatherable in the New World Database.")]
+    public async Task GatherableAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the gatherable to search for.", AutocompleteProviderType = typeof(GatherablesAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("shop", "Search for a shop in the New World Database.")]
+    public async Task ShopAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the shop to search for.", AutocompleteProviderType = typeof(ShopsAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("npc", "Search for an NPC in the New World Database.")]
+    public async Task NPCAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the NPC to search for.", AutocompleteProviderType = typeof(NPCSAutocompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("zone", "Search for a zone in the New World Database.")]
+    public async Task ZoneAsync(
+        [SlashCommandParameter(Name = "name", Description = "The name of the zone to search for.", AutocompleteProviderType = typeof(ZonesAutoCompleteProvider))]
+        string name
+    ) => await SendLink(name);
+
+    [SubSlashCommand("count", "Count the number of items in the New World Database.")]
+    public async Task CountAsync()
+    {
+        await RespondAsync(InteractionCallback.DeferredMessage(MessageFlags.Ephemeral));
+
+        using var db = new LiteDatabase("nwdb.db");
+        var collections = db.GetCollectionNames();
+        
+        // Get each collection.
+        var items = db.GetCollection<NWDB.Entity>("items").Count();
+        var mounts = db.GetCollection<NWDB.Entity>("mounts").Count();
+        var recipes = db.GetCollection<NWDB.Entity>("recipes").Count();
+        var abilities = db.GetCollection<NWDB.Entity>("abilities").Count();
+        var perks = db.GetCollection<NWDB.Entity>("perks").Count();
+        var statusEffects = db.GetCollection<NWDB.Entity>("statuseffects").Count();
+        var quests = db.GetCollection<NWDB.Entity>("quests").Count();
+        var creatures = db.GetCollection<NWDB.Entity>("creatures").Count();
+        var gatherables = db.GetCollection<NWDB.Entity>("gatherables").Count();
+        var shops = db.GetCollection<NWDB.Entity>("shops").Count();
+        var npcs = db.GetCollection<NWDB.Entity>("npcs").Count();
+        var zones = db.GetCollection<NWDB.Entity>("zones").Count();
+        var total = collections.Sum(c => db.GetCollection<NWDB.Entity>(c).Count());
+
+        await ModifyResponseAsync(msg =>
+        {
+            msg.Embeds = [
+                new EmbedProperties()
+                .WithTitle("New World Database Counts")
+                .WithColor(Colors.Pink)
+                .WithDescription(
+                $"**Items**: {items}\n" +
+                $"**Mounts**: {mounts}\n" +
+                $"**Recipes**: {recipes}\n" +
+                $"**Abilities**: {abilities}\n" +
+                $"**Perks**: {perks}\n" +
+                $"**Status Effects**: {statusEffects}\n" +
+                $"**Quests**: {quests}\n" +
+                $"**Creatures**: {creatures}\n" +
+                $"**Gatherables**: {gatherables}\n" +
+                $"**Shops**: {shops}\n" +
+                $"**NPCs**: {npcs}\n" +
+                $"**Zones**: {zones}\n\n" +
+                $"**Total**: {total}")
+                    ];
+        });
+    }
+
+    private async Task SendLink(string link)
+    {
+        await RespondAsync(
+            InteractionCallback.Message(
+                new InteractionMessageProperties().WithContent($"[Here!]({link})")
+            )
+        );
+    }
+}
+
+#region AutocompleteProviders
+static class NWDBAutoCompleteProvider
 {
     private static readonly LiteDatabase DB = new("nwdb.db");
 
-    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    public static ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(
+        ApplicationCommandInteractionDataOption option,
+        string collectionName)
     {
         var input = option.Value!;
-        
-        var items = DB.GetCollection<NWDB.Item>("items");
+
+        var items = DB.GetCollection<NWDB.Entity>(collectionName);
         var result = items.Find(d => d.Name.Contains(input), limit: 25).Take(25)
             .Select(d => new ApplicationCommandOptionChoiceProperties(d.Name, d.Url));
 
@@ -21,22 +156,100 @@ public class ItemAutocompleteProvider : IAutocompleteProvider<AutocompleteIntera
     }
 }
 
-[SlashCommand("nwdb", "New World Database commands.")]
-public partial class NewWorldDatabaseModule : ApplicationCommandModule<ApplicationCommandContext>
+public class ItemsAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
 {
-    #region Item Metadata
-    [SubSlashCommand("item", "Search for an item in the New World Database.")]
-    public partial Task ItemAsync(
-        [SlashCommandParameter(Name = "name", Description = "The name of the item to search for.", AutocompleteProviderType = typeof(ItemAutocompleteProvider))]
-        string item
-    );
-    #endregion
-    public partial async Task ItemAsync(string item)
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
     {
-        await RespondAsync(
-            InteractionCallback.Message(
-                new InteractionMessageProperties().WithContent($"[Here!]({item})")
-            )
-        );
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "items");
     }
 }
+
+public class MountsAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "mounts");
+    }
+}
+
+public class RecipesAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "recipes");
+    }
+}
+
+public class AbilitiesAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "abilities");
+    }
+}
+
+public class PerksAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "perks");
+    }
+}
+
+public class StatusEffectsAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "statuseffects");
+    }
+}
+
+public class QuestsAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "quests");
+    }
+}
+
+public class CreaturesAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "creatures");
+    }
+}
+
+public class GatherablesAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "gatherables");
+    }
+}
+
+public class ShopsAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "shops");
+    }
+}
+
+public class NPCSAutocompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "npcs");
+    }
+}
+
+public class ZonesAutoCompleteProvider : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    public ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        return NWDBAutoCompleteProvider.GetChoicesAsync(option, "zones");
+    }
+}
+
+#endregion
