1d3286145c813227cee9840ca93a7b5ef7cc7607
Graeme Morris <vmaxson.dev@gmail.com>
2025-01-24 19:21:24 -0700

    build: publish scraper for windows

--- publish.bat
+++ publish.bat
@@ -4,6 +4,7 @@ if not exist "bin" mkdir bin
 
 echo Building NWDB Scraper for linux-x64...
 dotnet publish "NWDB Scraper" -c Release -r linux-x64 /p:PublishSingleFile=true -o bin\nwdb
+dotnet publish "NWDB Scraper" -c Release -r win-x64 /p:PublishSingleFile=true -o bin\nwdb\win-x64
 
 echo.
 echo Building Artemis for linux-x64...
